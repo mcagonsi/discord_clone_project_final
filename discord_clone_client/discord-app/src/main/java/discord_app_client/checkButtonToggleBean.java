185e58940b1cdc4eacbb7ee36dd9922cf3fdb8c6
Matthew Brett <matthew.brett60@ed.cna.nl.ca>
2026-04-22 11:34:29 -0230

    Changed from ViewScoped to SessionScoped, adjusted some UI styling

--- discord_clone_client/discord-app/src/main/java/discord_app_client/checkButtonToggleBean.java
+++ discord_clone_client/discord-app/src/main/java/discord_app_client/checkButtonToggleBean.java
@@ -1,12 +1,12 @@
 package discord_app_client;
 
+
+import jakarta.inject.Named;
+import jakarta.enterprise.context.SessionScoped;
 import java.io.Serializable;
 
-import jakarta.faces.view.ViewScoped;
-import jakarta.inject.Named;
-
 @Named("checkButtonToggleBean")
-@ViewScoped
+@SessionScoped
 public class checkButtonToggleBean implements Serializable {
     
     private String activePanel = "create";
