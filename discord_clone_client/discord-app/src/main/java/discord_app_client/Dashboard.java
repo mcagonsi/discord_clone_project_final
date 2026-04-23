a1f6bdbdb522eae6da408f7ca169d2568fbddf1f
Michael Agonsi <144360839+mcagonsi@users.noreply.github.com>
2026-04-23 12:34:50 -0230

    Implement server rail and side navigation components; enhance dashboard functionality

--- discord_clone_client/discord-app/src/main/java/discord_app_client/Dashboard.java
+++ discord_clone_client/discord-app/src/main/java/discord_app_client/Dashboard.java
@@ -1,6 +1,8 @@
 package discord_app_client;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.List;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.SessionScoped;
@@ -12,12 +14,28 @@ public class Dashboard implements Serializable {
     private String title;
     private String MainMenuOption;
     private String MenuSubOption;
+    private String serverRailOption = "DMs";
     private boolean showProfile = false;
 
     public void toggleShowProfile() {
         this.showProfile = !this.showProfile;
         System.out.println("Show Profile: " + this.showProfile);
     }
+
+    public void toggleServerRailOption(String option) {
+        this.serverRailOption = option;
+        System.out.println("Server Rail Option: " + this.serverRailOption);
+    }
+
+    //just for testing
+    public List<String> getServers() {
+        return Arrays.asList("Server 1", "Server 2", "Server 3");
+    }
+  
+    public List<String> getDirectMessages() {
+        return Arrays.asList("DM 1", "DM 2", "DM 3");
+    }
+  
 
     public void setTitle(String title) {
         this.title = title;
@@ -49,6 +67,15 @@ public class Dashboard implements Serializable {
 
     public void setShowProfile(boolean showProfile) {
         this.showProfile = showProfile;
+    }
+
+    public void setServerRailOption(String serverRailOption) {
+        this.serverRailOption = serverRailOption;
+        
+    }
+
+    public String getServerRailOption() {
+        return serverRailOption;
     }
 
 }
